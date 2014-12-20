07a279f0bc5a6c375f584e47388d43d04b8a80b4
Lee Cooper <lee.cooper@lski.uk>
2014-12-20 15:34:53 +0000

    Init

---
+++ gulpfile.js
@@ -0,0 +1,67 @@
+var gulp = require('gulp'),
+    uglify = require('gulp-uglify'),
+    concat = require('gulp-concat'),
+    del = require('del'),
+    jeditor = require("gulp-json-editor"),
+    insert = require('gulp-insert'),
+    rename = require('gulp-rename'),
+    merge = require('merge-stream');
+
+var projectSettings = {
+    name: 'lski-request',
+    version: "1.0.0",
+    homepage: 'https://github.com/lski/lski-requestjs',
+    description: "A Promise based ajax request helper",
+    "author": "dev@lski.co.uk <dev@lski.co.uk>",
+    "keywords": [
+        "Promise",
+        "ajax",
+        "request"
+    ],
+    "license": "MIT",
+    "main": "dist/lski-request.js",
+};
+
+var packageOnlySettings = {
+    "repository": {
+        "url": "https://github.com/lski/lski-requestjs.git"
+    }
+};
+    
+gulp.task('clean', function(cb) {
+    del('dist', cb);
+});
+
+gulp.task('settings', function() {
+
+    var pkg = gulp.src("./package.json")
+        .pipe(jeditor(projectSettings))
+        .pipe(jeditor(packageOnlySettings))
+        .pipe(gulp.dest('./'));
+    
+    var bwr = gulp.src("./bower.json")
+        .pipe(jeditor(projectSettings))
+        .pipe(gulp.dest('./'));
+    
+    return merge(pkg, bwr);
+});
+
+gulp.task('dist', ['clean'], function() {
+
+    return gulp.src('src/*.js')
+        .pipe(concat(projectSettings.name + '.debug.js'))
+        .pipe(insert.prepend('/*!\n * ' + projectSettings.name + ' - ' + projectSettings.version + '\n*/\n'))
+        .pipe(gulp.dest('dist'));
+});
+
+gulp.task('minify-dist', ['dist'], function () {
+
+    return gulp.src('dist/*.js')
+        .pipe(uglify({ preserveComments: 'some' }))
+        .pipe(rename(function(path) {
+            path.basename = path.basename.replace(/\.[^.]*/, '');
+        }))
+        .pipe(gulp.dest('dist'));
+});
+
+gulp.task('default', ['settings', 'minify-dist'], function() {});
---
+++ test/spec/spec.js
@@ -0,0 +1,111 @@
+describe("main", function () {
+
+	beforeEach(function () {
+		lski.request.settings.dataType = lski.request.dataTypes.TEXT;
+	});
+
+	afterEach(function () {
+		lski.request.settings.dataType = lski.request.dataTypes.JSON;
+	});
+
+	it("namespace exists", function () {
+
+		console.log("namespace exists");
+		expect(lski.request).not.toBe(null);
+	});
+
+	it("basic send request via get", function (done) {
+
+		lski.request.send('http://api-echo.azurewebsites.net/ip', 'GET').then(function (response) {
+
+			console.log("basic send request via get", response);
+
+			expect(response.data).not.toBe(null);
+
+			var data = JSON.parse(response.data);
+
+			expect(data.ip).not.toBe(null);
+		})
+		.catch(genericCatch)
+		.then(done);
+	});
+
+	it("headers are correctly sent", function (done) {
+
+		lski.request.send('http://api-echo.azurewebsites.net/headers', 'GET', undefined, { headers: { 'accept': 'application/json' } }).then(function (response) {
+
+			console.log("headers are correctly sent", response);
+
+			expect(response).not.toBe(null);
+			expect(response.data).not.toBe(null);
+
+			var headers = JSON.parse(response.data);
+
+			expect(headers.accept).toBe('application/json');
+		})
+		.catch(genericCatch)
+		.then(done);
+	});
+
+	it("data sent correctly", function (done) {
+
+		lski.request.send('http://api-echo.azurewebsites.net/echo', 'POST', JSON.stringify({ a: 1, b: 'test' })).then(function (response) {
+
+			console.log(response, "data sent correctly");
+
+			expect(response).not.toBe(null);
+			expect(response.data).not.toBe(null);
+
+			var args = JSON.parse(response.data);
+
+			expect(args.a).toBe(1);
+		})
+		.catch(genericCatch)
+		.then(done);
+	});
+
+	it("handles 'not found' correctly (default)", function (done) {
+
+		lski.request.get('http://api-echo.azurewebsites.net/notFound').then(function (response) {
+
+			console.log("handles 'not found' correctly (default)", response);
+
+			expect(response && response.status).toBe(404);
+		})
+		.catch(shouldNotRun)
+		.then(done);
+	});
+
+	it("handles 'not found' correctly (rejectOnStatusCode: true)", function (done) {
+
+		lski.request.get('http://api-echo.azurewebsites.net/notFound', { rejectOnStatusCode: true })
+			.then(shouldNotRun)
+			.catch(function (err) {
+
+				console.log("handles 'not found' correctly (rejectOnStatusCode: true)", err);
+
+				expect(err && err.status).toBe(404);
+			})
+			.then(done);
+	});
+
+	/**
+	 * Generic function that records an error if run as sometimes we need to test a particular promise does not run a particular catch or then
+	 */
+	function shouldNotRun(response) {
+
+		expect(function () {
+			throw new Error("Should not hit here");
+		}).not.toThrow();
+	}
+
+	/**
+	 * Generic catch method for promises, shows an error and allows any following then/done to run normally
+	 */
+	function genericCatch(err) {
+
+		expect(function () {
+			throw new Error(err);
+		}).not.toThrow();
+	}
+});
